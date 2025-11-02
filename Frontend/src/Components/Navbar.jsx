abcf17f064f14c03f554b7e9bbb730d924dfe71d
mdjameel04 <jameeltony67@gmail.com>
2025-11-02 20:40:43 +0530

    inital commit

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
     <nav>
-      <div className='w-full flex items-center justify-between bg-black fixed px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 text-white'>
+      <div className='w-full flex items-center justify-between bg-green-500    fixed px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 text-white'>
         {/* logo */}
-        <div className=' flex items-center gap-2 mt-4'>
-  <img src="" alt="" />
+        <div className=' flex items-center justify-center gap-1 mt-4'>
+  <img src={assets.Logo} alt="" className='w-24 h-20 mt-2'/>
    <div>
-<h1>Veluxe Rentals </h1>
+<h1 className='font-medium '>Veluxe Rentals </h1>
    </div>
         </div>
 
--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,7 +1,13 @@
+import Logo from './Logo-img1.png'
 import video1 from './video_1.mp4'
 
 
 
 export const assets = {
-    video1
+    video1,
+    Logo
 }
+
+const NavItems =[
+    {name : "Home", }
+]
