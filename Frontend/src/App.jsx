4e850054b96e5b350ff1eeefc7f6059a186c0d35
mdjameel04 <jameeltony67@gmail.com>
2025-11-05 09:20:41 +0530

    featuers added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-// import Navbar from './Components/Navbar'
+import Navbar from './Components/Navbar'
 
 import { Routes,Route } from 'react-router-dom'
 import Carbooking from './Pages/Carbooking'
-// import Home from './Pages/Home'
+import Home from './Pages/Home'
 
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar/> */}
+      <Navbar/>
     <Routes>
-      {/* <Route path="/"  element={<Home/>}/> */}
+      <Route path="/"  element={<Home/>}/>
       <Route  path="/booking" element={<Carbooking/>} />
     </Routes>
 
